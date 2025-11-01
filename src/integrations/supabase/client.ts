a47bf31050a1aab330581f3ccee0c598e686d892
Mobiilihuolto <supabase@mobiilihuolto.com>
2025-11-02 00:47:41 +0200

    Updated client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://ouuwdlpteiqbhclmqusk.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im91dXdkbHB0ZWlxYmhjbG1xdXNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg0Mzc3NTQsImV4cCI6MjA3NDAxMzc1NH0.SiFIBfOAA2IGE1Y5MpXzt5bYDuM4FBJwlF1k0r8vGQA";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
